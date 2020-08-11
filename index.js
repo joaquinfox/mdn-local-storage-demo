9fd02ac062d17f8c750b4042debe1dc339808403
Joaquin Fox <Joaquin.Fox@gmail.com>
2020-08-11 10:17:57 -0700

    mvp

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-let bgcolorForm = document.querySelector();
+var htmlElem = document.querySelector('html');
+var pElem = document.querySelector('p');
+
+let bgcolorForm = document.querySelector('#bgcolor');
+let fontForm = document.querySelector('#font');
+
+if (!localStorage.getItem('bgcolor')) {
+  populateStorage();
+} else {
+  setStyles();
+}
 
 function populateStorage() {
   localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
+  localStorage.setItem('font', document.getElementById('font').value);
+
+  setStyles();
 }
+
+function setStyles() {
+  let currentColor = localStorage.getItem('bgcolor');
+  let currentFont = localStorage.getItem('font');
+
+  document.getElementById('bgcolor').value = currentColor;
+  document.getElementById('font').value = currentFont;
+
+  htmlElem.style.backgroundColor = '#' + currentColor;
+  pElem.style.fontFamily = currentFont;
+}
+
+bgcolorForm.onchange = populateStorage;
+fontForm.onchange = populateStorage;
